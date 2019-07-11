d7617482d1cebc46ed299199de1a710e3dbcdb8c
bubble <799673937.com>
2019-07-11 15:07:19 +0800

    first

---
+++ pages/air/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>机票首页</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="postcss" scoped>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>攻略首页</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="postcss" scoped>
+
+</style>
